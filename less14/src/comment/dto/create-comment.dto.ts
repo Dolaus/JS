2f5f00ffea8781df0a476ad1893437b064f6e071
Dolaus <dsadasdasdas19@gmail.com>
2024-11-28 23:54:38 +0200

    add validation

--- less14/src/comment/dto/create-comment.dto.ts
+++ less14/src/comment/dto/create-comment.dto.ts
@@ -1,6 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
+import {IsNotEmpty, IsString, MinLength} from "class-validator";
 
 export class CreateCommentDto {
     @ApiProperty({ example: 'Це мій коментар!', description: 'Текст коментаря' })
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(4, { message: 'має бути не менше 4 символів' })
     text: string;
 }
--- less14/src/exhibition/dto/create-exhibition.dto.ts
+++ less14/src/exhibition/dto/create-exhibition.dto.ts
@@ -1,6 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
+import {IsNotEmpty, IsString, MinLength} from "class-validator";
 
 export class CreateExhibitionDto {
     @ApiProperty({ example: 'A beautiful painting', description: 'Опис експоната' })
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(4, { message: 'має бути не менше 4 символів' })
     description: string;
 }
