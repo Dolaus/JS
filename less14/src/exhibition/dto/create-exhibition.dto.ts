a7bd9ae3465b1439f4cf7952fdd9f34c3ac52420
Dolaus <dsadasdasdas19@gmail.com>
2024-11-29 00:04:14 +0200

    fix

--- less14/src/exhibition/dto/create-exhibition.dto.ts
+++ less14/src/exhibition/dto/create-exhibition.dto.ts
@@ -1,6 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString, MinLength } from 'class-validator';
-import {IsFile} from "../../utils/file-validator.decorator";
+import {IsNotEmpty, IsString, MinLength} from "class-validator";
 
 export class CreateExhibitionDto {
     @ApiProperty({ example: 'A beautiful painting', description: 'Опис експоната' })
@@ -8,8 +7,4 @@ export class CreateExhibitionDto {
     @IsNotEmpty()
     @MinLength(4, { message: 'має бути не менше 4 символів' })
     description: string;
-
-    @ApiProperty({ type: 'string', format: 'binary', description: 'Файл зображення' })
-    @IsFile({ message: 'Файл має бути формату .jpeg або .png' })
-    image: any;
 }
